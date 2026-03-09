66ee3c8c6ba751fb57905b74d42755e2a0c2f757
Aayusha <ayushaghimireg@gmail.com>
2026-03-09 13:32:48 +0545

    UseToggle Hook completed

--- CustomHooks/src/App.jsx
+++ CustomHooks/src/App.jsx
@@ -1,9 +1,21 @@
 import './App.css'
-
+import useToggle from 'C:/React/CustomHooks/CustomHooks/src/UseToggle.jsx'
 function App() {
 
+  const[value, toggleValue] = useToggle(true)
+  console.log("Val----", value)
   return (
     <>
+    <div> 
+      <button onClick={toggleValue}>Toggle Heading</button>
+      <button onClick={()=> toggleValue(false)}>Hide Heading</button>
+      <button onClick={()=> toggleValue(true)}>Show Heading</button>
+      {/* <h1>This is Costom Hook</h1> */}
+
+      {
+        value? <h1>Custom Hooks in React</h1>:null
+      }
+    </div>
     </>
   )
 }
